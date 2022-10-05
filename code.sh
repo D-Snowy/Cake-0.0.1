b0dda892e495b565761b51db098f522f2a9c6d92
D-Snowy <115047918+D-Snowy@users.noreply.github.com>
2022-10-04 20:41:03 -0600

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,13 @@
+echo "Cake"
+echo "App:Coding"
+echo "Version 0.0.1"
+echo ""
+echo "Create A File"
+read file
+touch $file
+echo ""
+echo "done!"
+echo ""
+echo "choose an editor VI, VIM, NANO"
+read pigy
+$pigy $file
