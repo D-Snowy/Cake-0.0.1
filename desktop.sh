7b2d5b615758e6fbc1c25f6230f2c805d8fd097b
D-Snowy <115047918+D-Snowy@users.noreply.github.com>
2022-10-04 21:01:05 -0600

    Create desktop.sh

---
+++ desktop.sh
@@ -0,0 +1,11 @@
+echo "Cake"
+echo "App:Desktop"
+echo ""
+echo "code.sh is for coding"
+echo "calculator.sh is for math like education"
+echo "search.sh is for searching through files"
+echo "desktop.sh is to see what our programs are"
+echo ""
+echo "close?"
+read app
+./$app
