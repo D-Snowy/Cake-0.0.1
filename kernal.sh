a727cec11bd878654a0789f8f7a58c006d3dae3a
dcjr3 <96445806+dcjr3@users.noreply.github.com>
2022-10-04 20:48:24 -0600

    Create kernal.sh

---
+++ kernal.sh
@@ -0,0 +1,15 @@
+echo "Cake"
+echo ""
+echo "Version 0.0.1"
+echo "By Maysun Campos & D-Snowy"
+date
+echo ""
+echo "Welcome to the Cake operating system"
+echo "to use all our programs close this file and we'll explain what to do"
+echo "go to code.sh for coding go to search.sh to search go to desktop.sh for our programs"
+echo "go to calculator.sh for calculator"
+echo ""
+echo "also I never learned your name what is is?"
+read name
+echo "hello " $name
+echo "Copyright Cake 2022 All Rights Reserved"
